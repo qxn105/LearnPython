2e83b1805d363c313e89d687cf7922d0cf572c66
Aleksandr Bogdanov <qxn105@mail.ru>
2022-09-25 15:17:22 +0300

    Initial commit

---
+++ Глава 10 файлы и исключения/file_reader.py
@@ -0,0 +1,26 @@
+filename = 'pi_digits.txt'
+
+# Чтение файла целиком
+# with open(filename) as file_object:
+#     contents = file_object.read()
+# print(contents)
+
+# Чтение файла построчно
+# with open(filename) as file_object:
+#     for line in file_object:
+#         print(line, end='')
+
+# Создание списка строк по содержимому файла
+# with open(filename) as file_object:
+#     lines = file_object.readline()
+# print(lines)
+# for line in lines:
+#     print(line, end='')
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+pi_string = ''
+for line in lines:
+    pi_string += line.rstrip()
+print(pi_string[:10]) # вывод первых 10 символов
+print(len(pi_string))
---
+++ Глава 10 файлы и исключения/pi_string.py
@@ -0,0 +1,12 @@
+filename = 'pi_million_digits.txt'
+with open(filename) as file_object:
+    lines = file_object.readlines()
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+print(f"{pi_string[:52]}...")
+print(len(pi_string))
+
+my_birthday = '120372'
+say = 'есть' if my_birthday in pi_string else 'нет'
+print(f'В первом миллионе числа pi {say} {my_birthday}!')
