473310377643e12386180f7cd7edc8325e61c7b9
Aleksandr Bogdanov <qxn105@mail.ru>
2022-09-29 21:30:31 +0300

    ???

--- Глава 13  игра (продолжение)/Alien Invasion/settings.py
+++ Глава 13  игра (продолжение)/Alien Invasion/settings.py
@@ -3,7 +3,7 @@ class Settings:
     def __init__(self) -> None:
         """Инициализирует настройки игры."""
         # Параметры экрана
-        self.fullscreen_mode = 0
+        self.fullscreen_mode = 1
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
